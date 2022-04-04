196e358c6afdaf3d1c2783b426ea26a9dc4f50fd
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-04 09:18:58 +0800

    creates reusable component for Channels and DMs

---
+++ src/components/sidebar/ChannelDMs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ChannelDMsLayout } from "../styles";
+
+export default function ChannelDMs({ children }) {
+    return (
+        <ChannelDMsLayout>
+            { children }
+        </ChannelDMsLayout>
+    )
+}
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { SidebarStyles, GroupName } from "../styles";
-import { BsPencilSquare } from "react-icons/bs";
-import Channels from "./Channels";
-import DMs from "./DMs";
+import { BsPencilSquare, BsPlus, BsHash } from "react-icons/bs";
+import ChannelDMs from "./ChannelDMs";
 
 export default function Sidebar() {
     return (
@@ -12,8 +11,20 @@ export default function Sidebar() {
                 <BsPencilSquare />
             </GroupName>
             <div>
-                <Channels />
-                <DMs />
+                <ChannelDMs>
+                    <h2>Channels</h2>
+                    <ul>
+                        <li><BsHash /> batch17</li>
+                        <li><BsPlus /> Add channels</li>
+                    </ul>
+                </ChannelDMs>
+                <ChannelDMs>
+                    <h2>Direct Messages</h2>
+                    <ul>
+                        <li>Eloisa</li>
+                        <li>Slackbot</li>
+                    </ul>
+                </ChannelDMs>
             </div>
         </SidebarStyles>
     )
