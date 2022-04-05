e72798a607b3d4074afc4d9e0bb99ca5fd1790a5
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-05 21:07:52 +0800

    adds error validation to form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { FormContainer, FormLayout } from '../components/styles';
+import { FormContainer, FormLayout, Errors } from '../components/styles';
 
 export default function Login() {
+    const [userInfo, setUserInfo] = useState({
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState(false);
+    const [errorMessage, setErrorMessage] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://206.189.91.54/api/v1/auth/sign_in', userInfo)
+            .then(response => response)
+            .catch(error => {
+                const { data: { errors } } = error.response;
+                setErrors(true)
+                setErrorMessage(errors)
+                return
+            })
+
+    }
+
+    const handleChange = (e) => {
+        setErrors(false)
+        const key = e.target.id;
+        const value = e.target.value;
+        setUserInfo({
+            ...userInfo,
+            [key]: value
+        })
+    }
+
     return (
         <FormLayout>
             <FormContainer>
                 <h1>Sign In to Workspace</h1>
                 <p>Enter your <span>email</span> and <span>password</span></p>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <input 
                             type="email" 
                             id="email" 
                             aria-label="Email" 
-                            placeholder="Email" />
+                            placeholder="Email"
+                            value={userInfo.email} 
+                            onChange={handleChange}
+                            />
                     </div>
                     <div>
                         <input 
                             type="password" 
                             id="password" 
                             aria-label="Password" 
-                            placeholder="Password" />
+                            placeholder="Password"
+                            value={userInfo.password} 
+                            onChange={handleChange}
+                            />
+                        { errors &&  errorMessage.map((message, index) => <Errors key={index}>{message}</Errors>)}
                     </div>
                     <button type="submit">Sign In</button>
                 </form>
