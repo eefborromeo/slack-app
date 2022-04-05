ada6f88706855fd4e4d286c5bc5f5ac15578bf43
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-05 09:15:59 +0800

    creates Sign Up page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { FormContainer, FormLayout } from "../components/styles";
 
 export default function Signup() {
     return (
-        <div>
-            Signup
-        </div>
+        <FormLayout>
+            <FormContainer>
+                <h1>Sign Up</h1>
+                <p>Enter your <span>email</span> and <span>password</span></p>
+                <form>
+                    <div>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            aria-label="Email" 
+                            placeholder="Email" />
+                    </div>
+                    <div>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            aria-label="Password" 
+                            placeholder="Password" />
+                    </div>
+                    <div>
+                        <input 
+                            type="password" 
+                            id="password-confirm" 
+                            aria-label="Password Confirmation" 
+                            placeholder="Password Confirmation" />
+                    </div>
+                    <button type="submit">Sign In</button>
+                </form>
+            </FormContainer>
+        </FormLayout>
     )
 }
