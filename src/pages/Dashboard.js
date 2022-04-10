dac740969356a9dcc7c779f8b01f489eb4019a19
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-10 13:37:41 +0800

    adds routes for Messages

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,29 +1,51 @@
-import React, { useState, useContext} from "react";
-import { Navigate } from "react-router-dom";
+import React, { useState, useContext, useEffect} from "react";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import axios from "axios";
 import { UserContext } from "../contexts/User";
 
-import { FlexContainer } from "../components/styles";
+import { FlexContainer, MessageContainer, MessagesStyles, MessageLayout } from "../components/styles";
 import Sidebar from "../components/sidebar/Sidebar";
 import Messages from "../components/messages/Messages";
 import TopBar from "../components/topbar/TopBar";
 import { ReceiverContextProvider } from "../contexts/Receiver";
+import Title from "../components/messages/Title";
+
 
 export default function Dashboard() {
-    const { user } = useContext(UserContext);
-   const [allUsers, setAllUsers] = useState([]);
-   const [isNewMessage, setIsNewMessage] = useState(false);
-   
-   if (!user.isLoggedIn) {
-       return <Navigate to="/" />
-   }
+    const { user: { isLoggedIn, receivers} } = useContext(UserContext);
+    const [allUsers, setAllUsers] = useState([]);
+    const [isNewMessage, setIsNewMessage] = useState(false);
+ 
+    if (!isLoggedIn) {
+        return <Navigate to="/" />
+    }
+
     return (
         <>
             <TopBar />
             <FlexContainer>
-                <Sidebar setIsNewMessage={setIsNewMessage} setAllUsers={setAllUsers} />
-                <ReceiverContextProvider>
-                    <Messages allUsers={allUsers} isNewMessage={isNewMessage} setIsNewMessage={setIsNewMessage} />
-                </ReceiverContextProvider> 
+                    <Sidebar 
+                    setIsNewMessage={setIsNewMessage} 
+                    setAllUsers={setAllUsers}
+                    />
+                    <Routes>
+                        <Route path=":id" element={
+                            <Messages 
+                                allUsers={allUsers}
+                                isNewMessage={isNewMessage} 
+                                setIsNewMessage={setIsNewMessage} 
+                                />
+                        } />
+                        <Route path="/" element={
+                            <MessageContainer>
+                                  <Title 
+                                    allUsers={allUsers} 
+                                    isNewMessage={isNewMessage} 
+                                    setIsNewMessage={setIsNewMessage}
+                                    />
+                            </MessageContainer>
+                        } />
+                    </Routes>
             </FlexContainer>
         </>
     )
