fea2d140839f27bc0504e10d0cd7c53d27f28de1
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-18 11:36:42 +0800

    adds channel information about tab

--- src/components/modal/ChannelInfo.js
+++ src/components/modal/ChannelInfo.js
@@ -23,13 +23,18 @@ export default function ChannelInfo({ selectedChannel, getAllUsers, allUsers }) 
 
     const getMembersById = () => {
         const {channel_members} = channelInfo;
-        if (allUsers.length !== 0) {
-            const groupedMembers = allUsers.filter(user => channel_members.find(member => member.user_id === user.id))
-            setChannelMembers(groupedMembers)
-        }
-         
+        const groupedMembers = allUsers?.filter(user => channel_members?.find(member => member.user_id === user.id))
+        setChannelMembers(groupedMembers)
     }
 
+    const ownerInfo = allUsers?.find(user => channelInfo?.owner_id === user.id);
+
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    }
+    const date = new Date(channelInfo?.created_at).toLocaleDateString('en-US', options);
     
     useEffect(() => {
         getChannelInfo();
@@ -42,6 +47,16 @@ export default function ChannelInfo({ selectedChannel, getAllUsers, allUsers }) 
     return (
         <div>
             <h3>{channelInfo.name}</h3>
+            <ul>
+                <li>About</li>
+                <li>Members</li>
+            </ul>
+            <div>
+                <h3>About This Channel</h3>
+                <p>Owner: <span>{ownerInfo?.uid}</span></p>
+                <p>Date Created: <span>{date}</span></p>
+
+            </div>
             <div>
                 <h3>Channel Members</h3>
                 <ul>
