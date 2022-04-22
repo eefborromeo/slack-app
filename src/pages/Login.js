3606b4fb5f4ff33d7f777eee2a3615421e6dec0b
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-04-22 23:05:20 +0800

    uses usePost hook

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,47 +1,23 @@
-import React, { useState, useContext } from 'react';
-import axios from 'axios';
-import { Link, useNavigate, Navigate } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import { FormContainer, FormLayout, Errors } from '../components/styles';
 import { UserContext } from '../contexts/User';
+import usePost from '../hooks/usePost';
 
 export default function Login() {
 	const [userInfo, setUserInfo] = useState({
 		email: '',
 		password: '',
 	});
-	const [errors, setErrors] = useState(false);
 	const [errorMessage, setErrorMessage] = useState([]);
+	const { res, errors, handleSubmit } = usePost(
+		'http://206.189.91.54/api/v1/auth/sign_in',
+		userInfo
+	);
 	const { user, handleLogin } = useContext(UserContext);
 
-	const navigate = useNavigate();
-
-	const handleSubmit = e => {
-		e.preventDefault();
-
-		axios
-			.post('http://206.189.91.54/api/v1/auth/sign_in', userInfo)
-			.then(response => {
-				const {
-					data: {
-						data: { email, uid },
-					},
-					headers: { 'access-token': accessToken, client, expiry },
-				} = response;
-				handleLogin(uid, accessToken, client, email, expiry);
-				navigate('/app');
-			})
-			.catch(error => {
-				const {
-					data: { errors },
-				} = error.response;
-				setErrors(true);
-				setErrorMessage(errors);
-				return;
-			});
-	};
-
 	const handleChange = e => {
-		setErrors(false);
+		setErrorMessage([]);
 		const key = e.target.id;
 		const value = e.target.value;
 		setUserInfo({
@@ -49,6 +25,19 @@ export default function Login() {
 			[key]: value,
 		});
 	};
+
+	useEffect(() => {
+		if (res?.data && res?.headers) {
+			const {
+				data: {
+					data: { email, uid },
+				},
+				headers: { 'access-token': accessToken, client, expiry },
+			} = res;
+			handleLogin(uid, accessToken, client, email, expiry);
+		}
+		setErrorMessage(errors);
+	}, [res, errors, handleLogin]);
 
 	if (user.isLoggedIn) {
 		return <Navigate to="/app" />;
@@ -81,7 +70,9 @@ export default function Login() {
 							value={userInfo.password}
 							onChange={handleChange}
 						/>
-						{errors && errorMessage.map((message, index) => <Errors key={index}>{message}</Errors>)}
+						{errorMessage?.map((message, index) => (
+							<Errors key={index}>{message}</Errors>
+						))}
 					</div>
 					<button type="submit">Sign In</button>
 				</form>
